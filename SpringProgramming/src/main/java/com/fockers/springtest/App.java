ed005c169e887db03203020a994448379007762d
mike Jones <mikiej_boy@hotmail.com>
2015-11-24 19:20:29 +0000

    Spring programming setup - means to re-familiarize myself with spring
    Created spring starter program, obtained automatic dependencies for basic program.
    Created spring bean.xml configuration file - added Bean Person
    Created Bean POJO and a standard App.java
    
    App.java - created ApplicationContext and fetch bean.xml - this was then used to obtain bean person

---
+++ SpringProgramming/src/main/java/com/fockers/springtest/App.java
@@ -0,0 +1,34 @@
+package com.fockers.springtest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+
+public class App {
+
+	public static void main(String[] args) {
+
+//		Person person = new Person();
+//		person.speak();
+		
+		
+		//class to instantiate bean objects
+		ApplicationContext context;
+		context = new ClassPathXmlApplicationContext("com/fockers/springtest/beans/Beans.xml");
+		//no need to put path to bean.xml as it is in main project directory
+//		context = new FileSystemXmlApplicationContext("src/main/java/com/fockers/springtest/beans/Beans.xml");
+		
+		Person personSpring = (Person)context.getBean("person");
+		
+		personSpring.speak();
+		
+		
+		System.out.println("****Closing FileSystemXML*****");
+		//used to close down the connection + stop resource leaks
+		//((FileSystemXmlApplicationContext)context).close();
+		
+		
+	}
+
+}
---
+++ SpringProgramming/src/main/java/com/fockers/springtest/Person.java
@@ -0,0 +1,10 @@
+package com.fockers.springtest;
+
+public class Person {
+
+	public void speak()
+	{
+			System.out.println("hello i am a person");
+	}
+	
+}
